77c162c61fc0f6e4184a352419b1acf8d0d5f6a8
aziz <kuibeatboxer15@gmail.com>
2020-08-10 00:48:24 +0700

    add Login

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -108,54 +108,47 @@ function Home() {
   );
 }
 
-// function Login() {
-//   const [show, setShow] = useState(false);
+function Login() {
+  const [show, setShow] = useState(false);
 
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-//   return (
-//     <div>
-//       <Button className="login" variant="light" onClick={handleShow}>
-//         Login
-//       </Button>{" "}
-//       {/* Modal Login */}
-//       <Modal size="sm" show={show} onHide={handleClose}>
-//         <Modal.Body>
-//           <Form>
-//             <div className="modal-login-title">
-//               <h1>Login</h1>
-//             </div>
-//             <Form.Group controlId="formBasicEmail">
-//               <Form.Label className="text-email">Email</Form.Label>
-//               <Form.Control
-//                 className="size-text-email"
-//                 type="email"
-//                 placeholder="Enter email"
-//               />
-//             </Form.Group>
-//             <Form.Group controlId="formBasicPassword">
-//               <Form.Label className="text-password">Password</Form.Label>
-//               <Form.Control type="password" placeholder="Password" />
-//             </Form.Group>
-//             <Col sm={12}>
-//               <Button
-//                 variant="warning"
-//                 type="submit"
-//                 className="btn-login"
-//                 block
-//               >
-//                 Login
-//               </Button>
-//             </Col>
-//             <Form.Text className="text-muted center-text-account">
-//               Don't have an account? ? Clik Here !!!
-//             </Form.Text>
-//           </Form>
-//         </Modal.Body>
-//       </Modal>
-//     </div>
-//   );
-// }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div>
+      <Button className="login" variant="light" onClick={handleShow}>
+        Login
+      </Button>{" "}
+      {/* Modal Login */}
+      <Modal size="sm" show={show} onHide={handleClose}>
+        <Modal.Body>
+          <Form>
+            <div className="modal-login-title">
+              <h1>Login</h1>
+            </div>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="text-email">Email</Form.Label>
+              <Form.Control
+                className="size-text-email"
+                type="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="text-password">Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="warning" type="submit" className="btn-login" block>
+              Login
+            </Button>
+            <Form.Text className="text-muted center-text-account">
+              Don't have an account? ? Clik Here !!!
+            </Form.Text>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
 // function Register() {
 //   const [show, setShow] = useState(false);
 
@@ -204,7 +197,7 @@ function Home() {
 //               <Button
 //                 variant="warning"
 //                 type="submit"
-//                 className="btn-login"
+//                 className="btn-register-login"
 //                 block
 //               >
 //                 Login
@@ -228,8 +221,8 @@ function Header() {
           alt=""
         />
         <div className="btn-action">
-          {/* <Login></Login>
-          <Register></Register> */}
+          <Login></Login>
+          {/* <Register></Register> */}
         </div>
       </div>
     </div>
